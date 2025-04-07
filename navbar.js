fde1d32e15752c630428d67a0666715f1c22ca1b
MelvinButters <147711488+MelvinButters@users.noreply.github.com>
2025-04-07 22:56:37 +0930

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -6,7 +6,7 @@ navbar.innerHTML = `
     <nav>
         <ul>
             <li><a href="blog.melvinbutters.com">Home</a></li>
-            <li><a href="#">Articles</a></li>
+            <li><a href="archive.html">Articles</a></li>
             <li><a href="https://melvin-butters.itch.io/">Itch io</a></li>
             <li><a href="#">Patreon</a></li>
         </ul>
