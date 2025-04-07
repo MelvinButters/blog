892492546e7a1205370bd7d554e54b2580092578
MelvinButters <147711488+MelvinButters@users.noreply.github.com>
2025-04-07 22:59:59 +0930

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -5,7 +5,7 @@ const navbar = document.getElementById('navbar');
 navbar.innerHTML = `
     <nav>
         <ul>
-            <li><a href="blog.melvinbutters.com">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="archive.html">Articles</a></li>
             <li><a href="https://melvin-butters.itch.io/">Itch io</a></li>
             <li><a href="#">Patreon</a></li>
