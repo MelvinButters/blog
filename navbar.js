f5ff5d0c3d21be0ada73f560f28f9af7d3cf8d02
MelvinButters <147711488+MelvinButters@users.noreply.github.com>
2025-04-08 22:15:16 +0930

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -8,15 +8,4 @@ navbar.innerHTML = `
             <li><a href="contact.html">Contact</a></li>
         </ul>
     </nav>
-    <div class="hamburger" id="hamburger-menu">
-        &#9776; <!-- Hamburger icon -->
-    </div>
 `;
-
-// Adding mobile hamburger menu functionality
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const navLinks = document.querySelector('nav ul');
-
-hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggles the visibility of the nav on mobile
-});
